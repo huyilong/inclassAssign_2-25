fde99707cd191e88e89c319e8b69439f9e4781b8
hu.yilong <hu.yilong@wustl.edu>
2015-02-11 17:20:13 -0600

    php_session

---
+++ selfsubmit_form.php
@@ -0,0 +1,38 @@
+/*
+Oftentimes we don't want to pass form values to a different page, 
+but rather to the same page as contains the form so that we can process the information and 
+display output on that same page. To do this, you can simply omit the action parameter from 
+the form. Alternatively, you can use a PHP environment variable like $_SERVER['PHP_SELF'].
+
+
+The following example simply takes the data input from a form and, via POST variables, 
+echoes it back to the user as bold text on the same page:
+*/
+
+<!DOCTYPE html>
+<html>
+<head><title>Bold Printer</title></head>
+<body>
+
+
+<form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
+	<p>
+		<label for="name">Name:</label>
+		<input type="text" name="name" id="name" />
+	</p>
+	<p>
+		<input type="submit" value="Print in Bold" />
+	</p>
+</form>
+ 
+
+ 
+<?php
+if(isset($_POST['name'])){
+	printf("<p><strong>%s</strong></p>\n",
+		htmlentities($_POST['name'])
+	);
+}
+?>
+</body>
+</html>
---
+++ session.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+ 
+$oldnum = (int) @$_SESSION['inc_num'];
+if($oldnum<1) $oldnum=1;
+$newnum = $oldnum*2;
+ 
+echo $newnum;
+$_SESSION['inc_num'] = $newnum;
+?>
+
+
+
+
+/////////////Start a PHP Session
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Set session variables
+$_SESSION["favcolor"] = "green";
+$_SESSION["favanimal"] = "cat";
+echo "Session variables are set.";
+?>
+
+</body>
+</html>
+
+
+//////////Get PHP Session Variable Values
+
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Echo session variables that were set on previous page
+echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
+echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+?>
+
+</body>
+</html>
